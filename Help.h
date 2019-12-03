7d840fdef2df07915c5d39d1430111ff4957998f
ahipoki <50777654+ahipoki@users.noreply.github.com>
2019-12-03 07:39:49 -0800

    Create Help.h

---
+++ Help.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include "Command.h"
+
+class Help : public Command
+{
+  public:
+    Help(char*);
+    int getType();
+    char* returnBurn();
+    void printHelp(vector<Command*>*);
+  private:
+}
