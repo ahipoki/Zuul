2e15dde0c66da86f8cc61ba74851fe8733e303f3
ahipoki <50777654+ahipoki@users.noreply.github.com>
2019-12-02 21:37:49 -0800

    Create Inventory.cpp

---
+++ Inventory.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <iterator>
+#include "Inventory.h"
+#include "Item.h"
+
+Inventory::Inventory(char* newDescription) : Command(newDescription)
+{
+
+}
+
+int Inventory::getType()
+{
+  return 6;
+}
+
+char* Inventory::returnBurn(char* key)
+{
+  return key;
+}
+
+void Inventory::show(vector<Item*>* bag)
+{
+  vector<Item*>::iterator bagIt;
+  if (bag->begin() != bag->end())
+  {
+    for (bagIt = bag->begin(); bagIt != bag->end(); ++bagIt)
+    {
+      std::cout << (*bagIt)->getName() << " " << std::endl;
+    }
+    std::cout << std::endl;
+  }
+  else
+  {
+    std::cout << "You don't have any items" << std::endl;
+  }
+}
