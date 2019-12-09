a23d49e306dc498a100ed2be30cf5bf0863e6ae2
ahipoki <50777654+ahipoki@users.noreply.github.com>
2019-12-08 23:28:15 -0800

    Update Drop.cpp

--- Drop.cpp
+++ Drop.cpp
@@ -3,49 +3,43 @@
 #include <vector>
 #include <iterator>
 #include "Toss.h"
+#include "Item.h"
+#include "Room.h"
 
-Drop::Drop(char* newDescription) : Command(newDescription)
-{
-  
+Toss::Toss(char* newDesc) : Command(newDesc){
+
 }
 
-int Drop::getType()
-{
+int Toss::getType(){
   return 5;
 }
 
-char* Drop::returnBurn()
-{
-  return (char*) "You didn't drop any items";
+char* Toss::returnBurn(){
+  return (char*) "You didn't toss anything! #TeamTrees!";
 }
 
-void Drop::drop(Room** currentRoomptr, vector<Item*>* bag, char* itemName)
-{
+//Loop through current inventory items. If the name matches the Item name, create NEW item with desc in room item list, then DELETE the item from inventory.
+void Toss::drop(Room** currentRoomptr, vector<Item*>* bag, char* itemName){
   bool found = false;
   vector<Item*>::iterator inIt;
-  for(inIt = bag->begin(); inIt != bag->end(); ++inIt)
-  {
+  for(inIt = bag->begin(); inIt != bag->end(); ){
     char* inQ = (*inIt)->getName();
     char temp[strlen(inQ)+1] = "";
-    for (int i = 0; i < strlen(inQ); ++i)
-    {
-      temp[i] = tolower(inQ[i]);
+    for(int a = 0; a < strlen(inQ); ++a){
+      temp[a] = toupper(inQ[a]);
     }
-    if (strcmp((char*)temp, itemName) == 0)
-    {
+    if(strcmp((char*)temp, itemName) == 0){
       (*currentRoomptr)->addItem(inQ);
       delete (*inIt);
       inIt = bag->erase(inIt);
-      std::cout "You dropped " << inQ << std::endl;
+      cout << "Littered " << inQ << "!" << endl;
       found = true;
-    }
-    else
-    {
+    }else{
       ++inIt;
     }
   }
-  if (found == false)
-  {
-    std::cout << "You didn't drop anything" << std::endl;
+
+  if(found == false){
+    cout << "What are you aiming at!?!?" << endl;
   }
 }
