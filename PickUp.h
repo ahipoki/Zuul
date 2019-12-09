1844ddfced64c85e33555098578f7b9e58e0bd99
ahipoki <50777654+ahipoki@users.noreply.github.com>
2019-12-08 23:18:20 -0800

    Update PickUp.h

--- PickUp.h
+++ PickUp.h
@@ -1,16 +1,17 @@
-#include <cstring>
+//Grab is like anyother command but the player inventory, specified item to take, and the currentRoom are needed
 #include <iostream>
+#include <cstring>
 #include <vector>
 #include "Command.h"
 #include "Item.h"
 #include "Room.h"
 
-class PickUp : public Command
-{
-  public:
-    PickUp(char*);
-    int getType();
-    char* returnBurn();
-    void take(Room**, std::vector<Item*>*, char*);
-  private:
+class Grab : public Command {
+ public:
+  Grab(char*);
+  int getType();
+  char* returnBurn();
+  //Give: current room, player inventory, specified itemName 
+  void take(Room**, vector<Item*>*, char*);
+ private:
 };
