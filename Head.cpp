4dd09a5af770ed7eb98b335717ed71a46f2dc16c
ahipoki <50777654+ahipoki@users.noreply.github.com>
2019-12-10 22:53:50 -0800

    Update Head.cpp

--- Head.cpp
+++ Head.cpp
@@ -5,30 +5,37 @@
 #include "Head.h"
 #include "Room.h"
 
-Head::Head(char* newDescription) : Command(newDescription) {
+Head::Head(char* newDescription) : Command(newDescription)
+{
   
 }
 
-int Head::getType(){
+int Head::getType()
+{
   return 3;
 }
 
-char* Head::returnBurn(){
-  return (char*) "You head in the clouds. (Input cardinal direction)";
+char* Head::returnBurn()
+{
+  return (char*) "You can't move in no direction. Input a direction";
 }
 
 //The move just takes in the userin destination and iterates through the exits to try and find an exit room to set the currentRoom to 
-void Head::move(char* key, map<char*, Room*>* rm, Room **currentRoomptr){
+void Head::move(char* key, map<char*, Room*>* rm, Room **currentRoomptr)
+{
   map<char*, char*>::iterator eIt;
   map<char*, char*>* exits = (*currentRoomptr)->getExits();
   bool found = false;
-  for(eIt = exits->begin(); eIt != exits->end(); ++eIt){
-    if(strcmp(eIt->first, key) == 0){
+  for(eIt = exits->begin(); eIt != exits->end(); ++eIt)
+  {
+    if(strcmp(eIt->first, key) == 0)
+    {
       (*currentRoomptr) = ((*rm)[eIt->second]);
       found = true;
     }
   }
-  if(found == false){
-    cout << "No direction found!" << endl;
+  if(found == false)
+  {
+    cout << "That's not a valid direction!" << endl;
   }
 }
