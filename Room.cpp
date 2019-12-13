9399000e4e51fbb58e3767f1ed9faa91b04bc83b
ahipoki <50777654+ahipoki@users.noreply.github.com>
2019-12-13 01:04:02 -0800

    Update Room.cpp

--- Room.cpp
+++ Room.cpp
@@ -4,6 +4,26 @@
 
 using namespace std;
 
+Printable::~Printable()
+{
+  delete[] this->description;
+  delete[] this->name;
+}
+
+void Printable::print()
+{
+  cout << CC_BLD << CC_BLU << (this->name) << CC_CLR << endl;
+  cout << (this->description) << endl;
+}
+
+void Printable::init(char const* name, char const* description)
+{
+  this->description = new char[strlen(description) + 1];
+  this->name = new char[strlen(description) + 1];
+  strcpy(this->desription, description);
+  strcpy(this->name, name);
+}
+
 void Room::deleteItem(Item* toDelete)
 {
   vector<Item*>::iterator it = this->items.begin();
