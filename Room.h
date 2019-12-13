edef2f204b43f6f5801fa3eb46d46e092e693a8f
ahipoki <50777654+ahipoki@users.noreply.github.com>
2019-12-13 08:00:11 -0800

    Update Room.h

--- Room.h
+++ Room.h
@@ -7,13 +7,15 @@
 #define CC_GRN "\033[32m"
 #define CC_CLR "\033[0m"
 #define CC_CYN "\033[36m"
+//Colors
 
 using namespace std;
 
 const char* const DIRS[] = { "NORTH", "EAST", "SOUTH", "WEST" };
+//Directions
 
 class Printable
-{
+{//Printable class
   public:
    ~Printable();
    char* description;
@@ -23,12 +25,12 @@ class Printable
 };
 
 class Item : public Printable
-{
+{//Item class
 
 };
 
 class Room : public Printable 
-{
+{//Room class
  public:
   void printext();
   void printItem();
