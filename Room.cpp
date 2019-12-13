deb1846342712f462ebd9882bbe0f1e24ebbec9a
ahipoki <50777654+ahipoki@users.noreply.github.com>
2019-12-13 00:14:40 -0800

    Update Room.cpp

--- Room.cpp
+++ Room.cpp
@@ -4,48 +4,6 @@
 
 using namespace std;
 
-Printable::~Printable(){
-  delete[] this->description;
-  delete[] this->name;
-}
-
-void Printable::print()
-{
-  cout << CC_BLD << CC_BLU << (this->name) << CC_CLR << endl;
-  cout << (this->description) << endl;
-}
-
-void Printable::init(char const* name, char const* description)
-{
-  this->description = new char[strlen(description) + 1];
-  this->name = new char[strlen(name) + 1];
-  strcpy(this->description, description);
-  strcpy(this->name, name);
-}
-
-void Room::printext()
-{
-  cout << CC_GRN << "Exits: " << CC_CLR;
-  for (int i = 0; i < 4; i++)
-  {
-    if (this->rooms.count(i) > 0) {
-      cout << CC_CYN << (DIRS[i]) << CC_CLR << ": " << (this->rooms[i]->name) << " ";
-    }
-  }
-  cout << endl;
-}
-
-void Room::printItem()
-{
-  cout << CC_GRN << "Items: " << CC_CLR;
-  vector<Item*>::iterator it = this->items.begin();
-  while (it != this->items.end()) {
-    cout << ((*it)->name) << " ";
-    it++;
-  }
-  cout << endl;
-}
-
 void Room::deleteItem(Item* toDelete)
 {
   vector<Item*>::iterator it = this->items.begin();
