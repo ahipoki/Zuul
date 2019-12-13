6bc8e2439420c0609acbe31a9a3a7ea37e8abb10
ahipoki <50777654+ahipoki@users.noreply.github.com>
2019-12-12 16:01:27 -0800

    Update PickUp.cpp

--- PickUp.cpp
+++ PickUp.cpp
@@ -6,39 +6,49 @@
 #include "Item.h"
 #include "Room.h"
 
-Grab::Grab(char* newDescription) : Command(newDescription){
+PickUp::PickUp(char* newDescription) : Command(newDescription)
+{
 
 }
 
-int Grab::getType(){
+int PickUp::getType()
+{
   return 4;
 }
 
-char* Grab::returnBurn(){
+char* PickUp::returnBurn()
+{
   return (char*) "Congrats! You grabbed thin air!";
 }
 
 //Loop through current room items. If the name matches the Item name, create NEW item with desc in player inventory, then DELETE the item from the room.
-void Grab::take(Room** currentRoomptr, vector<Item*>* bag, char* itemName){
+void PickUp::take(Room** currentRoomptr, vector<Item*>* bag, char* itemName)
+{
   bool found = false;
   vector<Item*>::iterator rmIt;
-  for(rmIt = ((*currentRoomptr)->getItems())->begin(); rmIt != ((*currentRoomptr)->getItems())->end(); ){
+  for(rmIt = ((*currentRoomptr)->getItems())->begin(); rmIt != ((*currentRoomptr)->getItems())->end(); )
+  {
     char* inQ = (*rmIt)->getName();
     char temp[strlen(inQ)+1] = "";
-    for(int a = 0; a < strlen(inQ); ++a){
+    for(int a = 0; a < strlen(inQ); ++a)
+    {
       temp[a] = toupper(inQ[a]);
     }
-    if(strcmp((char*)temp, itemName) == 0){
+    if(strcmp((char*)temp, itemName) == 0)
+    {
       bag->push_back(new Item(inQ));
       delete (*rmIt);
       rmIt = ((*currentRoomptr)->getItems())->erase(rmIt);
       cout << "Received " << inQ << "!" << endl;
       found = true;
-    }else{
+    }
+    else
+    {
       ++rmIt;
     }
   }
-  if(found == false){
+  if(found == false)
+  {
     cout << "What are you grabbing at?" << endl;
   }
 }
