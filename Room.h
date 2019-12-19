314ca67c2013ca986ffdac924a65ae3459387c12
ahipoki <50777654+ahipoki@users.noreply.github.com>
2019-12-19 13:21:36 -0800

    Update Room.h

--- Room.h
+++ Room.h
@@ -7,39 +7,33 @@
 #define CC_GRN "\033[32m"
 #define CC_CLR "\033[0m"
 #define CC_CYN "\033[36m"
-//Colors
 
 using namespace std;
 
-/* Finn McKinley
- * A simple Zuul project
- * 12/13/19
- */
-
 const char* const DIRS[] = { "NORTH", "EAST", "SOUTH", "WEST" };
-//Directions
 
 class Printable
-{//Printable class
+{
   public:
-   ~Printable();
-   char* description;
-   char* name;
-   void print();
-   void init(char const* name, char const* description);
+    ~Printable();
+    char* description;
+    char* name;
+    void print();
+    void init(char const* name, char const* description);
 };
 
 class Item : public Printable
-{//Item class
+{
 
 };
 
-class Room : public Printable 
-{//Room class
- public:
-  void printext();
-  void printItem();
-  void deleteItem(Item* toDelete);
-  map<char, Room*> rooms;
-  vector<Item*> items;
+class Room : public Printable
+{
+  public:
+    void printext();
+    void printItem();
+    void deleteItem(Item* toDelete);
+    map<char, Room*> rooms;
+    vector<Item*> items;
+    char* title;
 };
